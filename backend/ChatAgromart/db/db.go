1fee163a6731caeb689d77555b6550ac9fad6d56
Kiraid <faizan.akhtar3130@gmail.com>
2025-04-07 07:01:48 +0500

    changed chat to mysql

--- backend/ChatAgromart/db/db.go
+++ backend/ChatAgromart/db/db.go
@@ -2,35 +2,44 @@ package db
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	"fmt"
 	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func InitDB() {
 	var err error
-	DB, err = sql.Open("sqlite3", "../api.db") // Assign to the global DB
+
+	// MySQL connection string: "user:password@tcp(host:port)/database"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("MYSQL_USER"), // Add this if needed, default is "root"
+		os.Getenv("MYSQL_ROOT_PASSWORD"),
+		os.Getenv("MYSQL_HOST_NAME"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DB"),
+	)
+
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Could not connect to database:", err)
 	}
-	_, err = DB.Exec("PRAGMA foreign_keys = ON;")
-	if err != nil {
-		log.Fatal("Could not enable foreign key support:", err)
-	}
-	
+
 	err = DB.Ping()
 	if err != nil {
 		log.Fatal("Could not establish a connection to the database:", err)
 	}
+
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 }
 
 func CreateTable() {
 
-
-createMessageTable := `CREATE TABLE IF NOT EXISTS messages (
+	createMessageTable := `CREATE TABLE IF NOT EXISTS messages (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	senderid INTEGER NOT NULL,
 	recieverid INTEGER NOT NULL,
@@ -38,24 +47,24 @@ createMessageTable := `CREATE TABLE IF NOT EXISTS messages (
 	time INTEGER NOT NULL
 
 );`
-//FOREIGN KEY (user1id) REFERENCES users(id) ON DELETE CASCADE,
-//FOREIGN KEY (user2id) REFERENCES users(id) ON DELETE CASCADE,
-//FOREIGN KEY (senderid) REFERENCES users(id) ON DELETE CASCADE
-_, err := DB.Exec(createMessageTable)
-if err != nil {
-	log.Fatalf("Error creating message table: %v", err)
-}
-
-// Create indexes for faster lookups
-createIndexes := []string{
-	"CREATE INDEX IF NOT EXISTS idx_recieverid ON messages (recieverid);",
-	"CREATE INDEX IF NOT EXISTS idx_senderid ON messages (senderid);",
-}
-
-for _, query := range createIndexes {
-	_, err = DB.Exec(query)
+	// FOREIGN KEY (user1id) REFERENCES users(id) ON DELETE CASCADE,
+	// FOREIGN KEY (user2id) REFERENCES users(id) ON DELETE CASCADE,
+	// FOREIGN KEY (senderid) REFERENCES users(id) ON DELETE CASCADE
+	_, err := DB.Exec(createMessageTable)
 	if err != nil {
-		log.Fatalf("Error creating index: %v", err)
+		log.Fatalf("Error creating message table: %v", err)
 	}
-}
+
+	// Create indexes for faster lookups
+	createIndexes := []string{
+		"CREATE INDEX IF NOT EXISTS idx_recieverid ON messages (recieverid);",
+		"CREATE INDEX IF NOT EXISTS idx_senderid ON messages (senderid);",
+	}
+
+	for _, query := range createIndexes {
+		_, err = DB.Exec(query)
+		if err != nil {
+			log.Fatalf("Error creating index: %v", err)
+		}
+	}
 }
